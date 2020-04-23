d82083282ca8e0c4fa3d195b4ba3e08a167d950f
Xiang Yu <yuxiang@Xiangs-MacBook-Air.local>
2020-04-22 21:14:35 -0500

    chapter 2 studio

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -1,7 +1,14 @@
 package org.launchcode.java.studios.areaofacircle;
+import java.util.Scanner;
 
 public class Area {
-    public static void main() {
-
+    public static void main(String[] args) {
+        double radius;
+        double area;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a radius: ");
+        radius = input.nextDouble();
+        area = Circle.getArea(radius);
+        System.out.println("The area of a circle of radius " + radius + " is: " + area);
     }
 }
