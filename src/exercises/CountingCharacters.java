ade6c2ad8985156211d1157b9ff0883e46e233c9
Xiang Yu <yuxiang@Xiangs-MacBook-Air.local>
2020-04-24 20:35:15 -0500

    Chapter 3 studio with bonus

--- src/exercises/CountingCharacters.java
+++ src/exercises/CountingCharacters.java
@@ -1,25 +1,31 @@
 package exercises;
 import java.util.HashMap;
-import java.util.ArrayList;
+import java.util.Map;
+import java.util.Scanner;
+
+import static java.lang.Character.*;
 
 public class CountingCharacters {
     public static void main(String[] args) {
-        String quote = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
-        char[] charactersInQuote = quote.toCharArray();
-//        System.out.println(charactersInQuote);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String quote = input.nextLine().toLowerCase();
+        input.close();
+        char[] characterArray = quote.toCharArray();
+        //        String quote = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
 
-        HashMap<String, Double> counting = new HashMap();
+        HashMap<Character, Integer> countingMap = new HashMap<>();
 
-        ArrayList<Character> characters = new ArrayList<>();
-
-        for (char eachCharacter : charactersInQuote) {
-            if (!characters.contains(eachCharacter)) {
-                characters.add(eachCharacter);
+        for (char each : characterArray) {
+            if (!countingMap.containsKey(each) && Character.isLetter(each)) {
+                countingMap.put(each, 1);
+            } else if (countingMap.containsKey(each) && Character.isLetter(each)) {
+                countingMap.put(each, countingMap.get(each)+1);
             }
-
-            System.out.println(characters);
-
         }
 
+        for (Map.Entry<Character, Integer> pair : countingMap.entrySet()) {
+            System.out.println(pair.getKey() + ": " + pair.getValue());
+        }
     }
 }
